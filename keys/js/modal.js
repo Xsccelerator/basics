8b23dd34982868d58d58bff119082bdd1a8ae9f4
Oleksii Kravets <kravets.oleksii.noone@gmail.com>
2020-05-03 20:54:35 +0200

    modal optimize

--- keys/js/modal.js
+++ keys/js/modal.js
@@ -1,44 +1,38 @@
- document.querySelectorAll('.modal-show').forEach(function(element){ //Получаем массив кнопок открытия
-     element.onclick = showModal;
- });
 
- document.querySelectorAll('.modal-close').forEach(function(element){ //Получаем массив кнопок закрытия
-     element.onclick = closeModal;
- })
- document.querySelectorAll('.modal-wrap').forEach(function(element){ //Получаем массив кнопок закрытия
-     element.onclick = closeModalWrap;
+(function(){
+    document.querySelectorAll('.modal-show').forEach(function(element){ //Получаем массив кнопок открытия
+        element.onclick = showModal;
+    });
+   
+    document.querySelectorAll('.modal-close').forEach(function(element){ //Получаем массив кнопок закрытия
+        element.onclick = closeModal;
+    })
+    document.querySelectorAll('.modal-wrap').forEach(function(element){ //Получаем модальное окно
+        element.onclick = closeModal;
+       
+    })
+   
+   
+   
+   
+    function showModal(){
+        let modalId = this.dataset.modal //связываем кнопку с модальным окном по id
+        document.querySelector(modalId).classList.remove('hide')
+        document.onkeydown = function(e){
+           if(event.keyCode === 27) closeModal()
+              
+           }
+       }
     
- })
-
-
-
-
- function showModal(){
-     let modalId = this.dataset.modal //связываем кнопку с модальным окном по id
-     document.querySelector(modalId).parentElement.classList.remove('hide')
-     document.querySelector(modalId).classList.remove('hide')
-     document.onkeydown = function(e){
-        if(event.keyCode === 27){
-           document.querySelectorAll('.modal-wrap').forEach(function(element){ //Получаем массив кнопок закрытия
-               element.classList.add('hide')
-               element.children[0].classList.add('hide')
-               console.log('hi')
-               document.onkeydown = null;
-           })
-        }
-    }
- };
- function closeModal(){
-    let modalId = this.dataset.modal //связываем кнопку с модальным окном по id
-    document.querySelector(modalId).parentElement.classList.add('hide')
-    document.querySelector(modalId).classList.add('hide')
+    function closeModal(){
+       document.querySelectorAll('.modal-wrap').forEach(function(element){ 
+           element.classList.add('hide')
+           
+   
+    })
     document.onkeydown = null;
-
- }
- function closeModalWrap(){
-    this.classList.add('hide')
-    this.children[0].classList.add('hide')
-    document.onkeydown = null;
-
- }
+   }
+   console.log(this)
+   
+})()
 
