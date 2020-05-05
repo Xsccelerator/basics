b5f06b9050ef7c9110b6a0cab006b82f255c5840
Oleksii Kravets <kravets.oleksii.noone@gmail.com>
2020-05-05 12:44:28 +0200

    simple-form-slider

---
+++ slider/js/slider.js
@@ -0,0 +1,6 @@
+document.querySelector('.show-next').onclick = function(){
+    document.querySelector('.form-slider').style.marginLeft = '-250px'
+}
+document.querySelector('.show-prev').onclick = function(){
+    document.querySelector('.form-slider').style.marginLeft = '0px'
+}
